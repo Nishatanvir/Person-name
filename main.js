ee2fb58018e3679ff6c91e8835fbd726e0c39b69
Nishatanvir <160606544+Nishatanvir@users.noreply.github.com>
2024-03-02 00:23:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let Person_name = "Nisha";
+console.log("Hello", Person_name, "would you like to learn some Python today?");
---
+++ main.ts
@@ -0,0 +1,3 @@
+let Person_name: string = "Nisha";
+
+console.log("Hello",Person_name,"would you like to learn some Python today?");
